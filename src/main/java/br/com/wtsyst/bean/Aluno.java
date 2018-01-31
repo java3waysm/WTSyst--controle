3691f530af98f9e028ad69272b1fc625d0b29105
thiago <thiago@thiago.thiago>
2018-01-30 22:00:53 -0200

    alterações

--- src/main/java/br/com/wtsyst/bean/Aluno.java
+++ src/main/java/br/com/wtsyst/bean/Aluno.java
@@ -2,10 +2,5 @@ package br.com.wtsyst.bean;
 
 public class Aluno {
 	private String nome;
-	private String matricula;
-	private String nomeMae;
-	private String nomePai;
-	private Double altura;
-	private String peso;
-	private String apelido; 
+	
 }
