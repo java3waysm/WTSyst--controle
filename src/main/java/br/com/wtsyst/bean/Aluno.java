e9ef187be4996bebc1625b1a9a816fe46df72c22
Erivan <erivan.nas@gmail.com>
2018-02-03 08:33:18 -0200

    exemplo de commit and push

--- src/main/java/br/com/wtsyst/bean/Aluno.java
+++ src/main/java/br/com/wtsyst/bean/Aluno.java
@@ -8,4 +8,46 @@ public class Aluno {
 	private Double peso;
 	private String codigo;
 	private String matricula;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getNomeMae() {
+		return nomeMae;
+	}
+	public void setNomeMae(String nomeMae) {
+		this.nomeMae = nomeMae;
+	}
+	public String getNomePai() {
+		return nomePai;
+	}
+	public void setNomePai(String nomePai) {
+		this.nomePai = nomePai;
+	}
+	public Double getAltura() {
+		return altura;
+	}
+	public void setAltura(Double altura) {
+		this.altura = altura;
+	}
+	public Double getPeso() {
+		return peso;
+	}
+	public void setPeso(Double peso) {
+		this.peso = peso;
+	}
+	public String getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+	public String getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
 }
