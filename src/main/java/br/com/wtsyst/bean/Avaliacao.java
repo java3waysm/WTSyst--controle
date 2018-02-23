b9f7f1c982fdc124e9edc5ba873521f81649ec08
thiago <thiago@thiago.thiago>
2018-02-23 16:36:53 -0300

    entidade avaliação

---
+++ src/main/java/br/com/wtsyst/bean/Avaliacao.java
@@ -0,0 +1,34 @@
+package br.com.wtsyst.bean;
+
+public class Avaliacao {
+	
+	private Integer id;
+	private Curso curso;
+	private Aluno aluno;
+	private Double nota;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Curso getCurso() {
+		return curso;
+	}
+	public void setCurso(Curso curso) {
+		this.curso = curso;
+	}
+	public Aluno getAluno() {
+		return aluno;
+	}
+	public void setAluno(Aluno aluno) {
+		this.aluno = aluno;
+	}
+	public Double getNota() {
+		return nota;
+	}
+	public void setNota(Double nota) {
+		this.nota = nota;
+	}
+}
