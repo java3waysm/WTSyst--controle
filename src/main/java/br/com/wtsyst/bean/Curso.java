f436d3bff2bff178aae0384a96d8e38279f14135
Fernando de Pinho Araujo <o.megapinho@gmail.com>
2018-02-24 09:57:45 -0300

    Nova tentativa de correção da endidade curso

--- src/main/java/br/com/wtsyst/bean/Curso.java
+++ src/main/java/br/com/wtsyst/bean/Curso.java
@@ -3,29 +3,17 @@ package br.com.wtsyst.bean;
 import java.util.List;
 
 import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 
 public class Curso {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_curso")
 	private Integer id;
 	private String nome;
-	@OneToOne
 	
 	private Professor professor;
-
-	@OneToMany(targetEntity = Curso.class, mappedBy = "curso", fetch = FetchType.LAZY)	
 	
 	@ManyToMany(fetch = FetchType.LAZY, targetEntity = Aluno.class, mappedBy = "cursos")
 	private List<Aluno> alunos;
-	@ManyToOne
 	
 	private Escola escola;
 	
